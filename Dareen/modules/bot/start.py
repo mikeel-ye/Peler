3e2fc5251a04ede1be7fd385c19ca21f63baa97a
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-27 21:23:56 +0700

    Add files via upload

---
+++ Dareen/modules/bot/start.py
@@ -0,0 +1,46 @@
+# if you can read this, this meant you use code from Geez | Ram Project
+# this code is from somewhere else
+# please dont hestitate to steal it
+# because Geez and Ram doesn't care about credit
+# at least we are know as well
+# who Geez and Ram is
+#
+#
+# kopas repo dan hapus credit, ga akan jadikan lu seorang developer
+# ©2023 Geez | Ram Team
+import random
+from Dareen import app
+from pyrogram import filters
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from config import OWNER_ID as owner 
+
+@app.on_callback_query()
+def pmowner(client, callback_query):
+    user_id = owner
+    message = "Lu Siapa Anjeng!!!!"
+    client.send_message(user_id, message)
+    client.answer_callback_query(callback_query.id, text="Message sent")
+
+logodareen = [
+    "https://telegra.ph//file/067dbc0672a362063cd7b.jpg"
+]
+
+alive_logo = random.choice(logodareen)
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(app, message):
+    chat_id = message.chat.id
+    file_id = alive_logo
+    caption = "Hi, Saya Asisstant Dareen-Userbot\nApaa Broo? Mau Marah Emang Punya Tatto?."
+    reply_markup = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("Support", url="https://t.me/darensupport"),
+            InlineKeyboardButton("Channel", url="https://t.me/cehadaren"),
+            InlineKeyboardButton("Owner", url="https://t.me/Darenrorr"),
+        ],
+    ])
+
+    await app.send_photo(chat_id, file_id, caption=caption, reply_markup=reply_markup)
